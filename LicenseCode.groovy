8c3e8224536616a8b65f739054a5b40834070323
Raul Pelaez <raulpelaez@gmail.com>
2020-06-23 11:58:27 +0200

    JJ LicenseCode.groovy

---
+++ LicenseCode.groovy
@@ -0,0 +1,44 @@
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.user.util.UserUtil
+import com.atlassian.jira.event.AbstractEvent
+import com.atlassian.crowd.event.user.UserCreatedEvent
+import com.atlassian.jira.application.ApplicationAuthorizationService
+import com.atlassian.jira.application.ApplicationKeys
+
+
+UserUtil userUtil = ComponentAccessor.getUserUtil()
+
+int licenses = 0
+
+def CurrentUser = ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser()
+def applicationAuthorizationService = ComponentAccessor.getComponent(ApplicationAuthorizationService)
+
+ 
+def total = 0
+def output
+def lstjson = []
+
+ 
+userUtil.getUsers().each{ u ->
+    if ((u.active) && (applicationAuthorizationService.canUseApplication(u, ApplicationKeys.SERVICE_DESK)))
+    {
+        //get the groups of the user
+        def groupManager = ComponentAccessor.getGroupManager()
+        def groupsNamesForUser = groupManager.getGroupNamesForUser(u)
+
+        output='''{
+        "user": "'''+u.emailAddress+'''",
+        "displayName": "'''+u.displayName+'''"
+        }'''
+
+        licenses++
+        lstjson.add(output)
+    }
+    def json= '''{ "items": '''+ lstjson +''' }'''
+    log.warn("json"+json)
+
+  //  File file = new File("//cifs//EEHQ//licenses.json")
+  //  file.write(json)
+}
+
+return licenses
